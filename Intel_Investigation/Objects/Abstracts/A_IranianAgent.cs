9d23e7346ea852e4f428228ce80cd9586bad23f4
David-396 <david.eini12@gmail.com>
2025-06-15 11:38:48 +0300

    abstract iranian agent

--- Intel_Investigation/Objects/Abstracts/A_IranianAgent.cs
+++ Intel_Investigation/Objects/Abstracts/A_IranianAgent.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Intel_Investigation.Enums;
 
 namespace Intel_Investigation.Objects.Abstracts
 {
-    internal class A_IranianAgent
+    abstract class A_IranianAgent
     {
+        protected AgentRank AgentRank;
+        protected int SensorsNumber;
+        protected AgentRank[] AgentRanksList;
 
+
+        public A_IranianAgent(AgentRank agentRank, AgentRank[] agentRanksList, int sensorsNumber)
+        {
+            this.AgentRank = agentRank;
+            this.AgentRanksList = agentRanksList;
+            this.SensorsNumber = sensorsNumber;
+        }
     }
 }
