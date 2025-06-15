69d07513c7b1dffcd55d51ffc57da3f83480c387
David-396 <david.eini12@gmail.com>
2025-06-15 11:20:34 +0300

    Add project files.

---
+++ Intel_Investigation/Objects/Abstracts/A_Sensor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intel_Investigation.Objects.Abstracts
+{
+    internal class A_Sensor
+    {
+    }
+}
---
+++ Intel_Investigation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
